a960fa054245b39d88af2ff2a52d40e2cfbaba9e
yttiktak <repeatingshadow@protonmail.com>
2018-11-19 23:11:54 -0500

    no gpufence here. Use bariers in compute shader

--- Assets/Cloud.cs
+++ Assets/Cloud.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 public enum Methods {
-	power,sph,gravity
+	power,sph,gravity,smooth
 };
 
 public class Cloud : MonoBehaviour {
@@ -12,30 +12,21 @@ public class Cloud : MonoBehaviour {
 	public Methods methods;
 	public Material material;
     public ComputeShader cloudCompute;
+	public float particleRadius = 0.5f;
 	public float particleMass = 1.0f;
-	public float particleRestDensity = 1.0f;
+	public float particleRestDensity = 10f;
 	public float particleStiffness = 2000f;
 	public float particleViscosity = 3000f;
-	public float smoothingLength = 0.5f;
-	/**
-	 * 		Shader.SetGlobalFloat("particleMass",1f);
-		Shader.SetGlobalFloat("particleRadius",0.5f);
-		Shader.SetGlobalFloat("particleRestDensity",1.0f);
-		Shader.SetGlobalFloat("particleStiffness",2000.0f);
-		Shader.SetGlobalFloat("particleViscosity",3000.0f);
-		Shader.SetGlobalFloat("smoothingLength",slen);
-		Shader.SetGlobalFloat("smoothingLength3",Mathf.Pow(slen,3));
-		Shader.SetGlobalFloat("smoothingLength6",Mathf.Pow(slen,6));
-		Shader.SetGlobalFloat("time_step",0.005f);
-	 * **/
+	public float smoothingLength = 40f;
 
-	int csidDensityPressure;
-	int csidPressureForce;
 	int csidGravity;
 	int csidPower;
-	int csidIntegrate;
-    int nthr = 10;
-	int npts = 1024 * 64; // 100 mill is too big!
+	int csidSPH;
+	int csidSmoothBall;
+
+    int nthr;
+	int ngrps;
+	int npts = 512 * 12; // 100 mill is too big!
 	ComputeBuffer compute_buffer;
 
 	struct Particle
@@ -49,7 +40,7 @@ public class Cloud : MonoBehaviour {
 	}
 
 	void Start () {
-        npts = 1024 * nthr;
+		uint ngx,ngy,ngz; // groups declared in the compute shader
         float rad;
         float phi;
         float theta;
@@ -58,86 +49,100 @@ public class Cloud : MonoBehaviour {
 			maxRad = 50.0f;
 		}
 		compute_buffer = new ComputeBuffer (npts, sizeof(float) * 14, ComputeBufferType.Default);
-		csidDensityPressure = cloudCompute.FindKernel ("DensityPressure");
-		csidPressureForce = cloudCompute.FindKernel ("PressureForce");
-		csidGravity = cloudCompute.FindKernel ("Gravity");
-		csidPower = cloudCompute.FindKernel ("Power");
-		csidIntegrate = cloudCompute.FindKernel ("Integrate");
 
+	//	csidDensityPressure = cloudCompute.FindKernel ("DensityPressure");
+	//	csidPressureForce = cloudCompute.FindKernel ("PressureForce");
+	//	csidGravity = cloudCompute.FindKernel ("Gravity");
+	//	csidPower = cloudCompute.FindKernel ("Power");
+	//	csidIntegrate = cloudCompute.FindKernel ("Integrate");
+
+		csidSPH = cloudCompute.FindKernel ("Sph");
+		csidPower = cloudCompute.FindKernel ("Power");
+		csidGravity = cloudCompute.FindKernel ("Gravity");
+		csidSmoothBall = cloudCompute.FindKernel ("SmoothBall");
+
+		cloudCompute.GetKernelThreadGroupSizes (csidSPH, out ngx, out ngy,out  ngz); // just trust the same in other kernels
+		nthr = npts /(int) ngx;
+		Debug.Log (nthr);
 		Particle[] cloud = new Particle[npts];
 		for (uint i = 0; i < npts; ++i) {
 			cloud [i] = new Particle ();
-			rad = maxRad *  Random.Range(0f, 0.99f)*Random.Range(0f,0.99f);
+			if (methods == Methods.power) {
+				rad = maxRad;
+			} else {
+				rad = maxRad * Mathf.Sqrt (Random.Range (0f, 1.0f));
+			}
             theta = Random.Range(0f, 3.1415926535f* 2f);
             phi = Mathf.Acos( Random.Range(0f, 2f) -1);
             // See https://www.bogotobogo.com/Algorithms/uniform_distribution_sphere.php
             cloud[i].position = new Vector3(rad * Mathf.Cos(phi), rad * Mathf.Sin(theta) * Mathf.Sin(phi), rad * Mathf.Cos(theta) * Mathf.Sin(phi));
-			cloud [i].velocity = new Vector3 (0, 0, 0); //Random.Range (-0.01f, 0.01f), Random.Range (-0.01f, 0.01f), Random.Range (-0.01f, 0.01f));
+			cloud [i].velocity = new Vector3 (0, 0, 0); // 200000.0f *  new Vector3 (Random.Range (-1f, 1f), Random.Range (-1f, 1f), Random.Range (-1f, 1f));
 			cloud [i].force = new Vector3 (0,0,0); //(Random.Range (-1f, 1), Random.Range (-1f, 1f), Random.Range (-1f, 1f));
-			cloud [i].color = new Vector3(0,1,1);
+			cloud [i].color = new Vector3(0,0.5f,1);
 			cloud [i].density = 100;
 			cloud [i].pressure = 0;
 		}
 			
 		Shader.SetGlobalFloat("particleMass",particleMass);
-		Shader.SetGlobalFloat("particleRadius",0.5f);
+		Shader.SetGlobalFloat("particleRadius",particleRadius);
 		Shader.SetGlobalFloat("particleRestDensity",particleRestDensity);
 		Shader.SetGlobalFloat("particleStiffness",particleStiffness);
 		Shader.SetGlobalFloat("particleViscosity",particleViscosity);
 		Shader.SetGlobalFloat("smoothingLength",smoothingLength);
+		Shader.SetGlobalFloat("smoothingLength2",Mathf.Pow(smoothingLength,2));
 		Shader.SetGlobalFloat("smoothingLength3",Mathf.Pow(smoothingLength,3));
 		Shader.SetGlobalFloat("smoothingLength6",Mathf.Pow(smoothingLength,6));
-		Shader.SetGlobalFloat("time_step",0.0001f);
+		Shader.SetGlobalFloat ("smoothingLength9", Mathf.Pow (smoothingLength, 9));
+		if (methods == Methods.sph) {
+			Shader.SetGlobalFloat ("time_step", 0.0001f);
+		} else {
+			Shader.SetGlobalFloat ("time_step", 0.051f);			
+		}
 
 
 
 		compute_buffer.SetData (cloud);
         int cloudID = Shader.PropertyToID("cloud");
-		cloudCompute.SetBuffer(csidDensityPressure, cloudID, compute_buffer);
-		cloudCompute.SetBuffer(csidPressureForce, cloudID, compute_buffer);
-		cloudCompute.SetBuffer(csidGravity, cloudID, compute_buffer);
-		cloudCompute.SetBuffer(csidPower, cloudID, compute_buffer);
-		cloudCompute.SetBuffer(csidIntegrate, cloudID, compute_buffer);
+	//	cloudCompute.SetBuffer(csidDensityPressure, cloudID, compute_buffer);
+	//	cloudCompute.SetBuffer(csidPressureForce, cloudID, compute_buffer);
+	//	cloudCompute.SetBuffer(csidGravity, cloudID, compute_buffer);
+	//	cloudCompute.SetBuffer(csidPower, cloudID, compute_buffer);
+		cloudCompute.SetBuffer(csidSPH, cloudID, compute_buffer);
 
         Shader.SetGlobalBuffer(cloudID, compute_buffer);
         Graphics.SetRandomWriteTarget(1, compute_buffer,true);
-        Shader.SetGlobalInt("_npts", npts);// twice?? but isnt it global??
+        Shader.SetGlobalInt("_npts", npts);
 
     }
 	
 	void OnValidate() {
 		Shader.SetGlobalFloat("particleMass",particleMass);
-		Shader.SetGlobalFloat("particleRadius",0.5f);
+		Shader.SetGlobalFloat("particleRadius",particleRadius);
 		Shader.SetGlobalFloat("particleRestDensity",particleRestDensity);
 		Shader.SetGlobalFloat("particleStiffness",particleStiffness);
 		Shader.SetGlobalFloat("particleViscosity",particleViscosity);
 		Shader.SetGlobalFloat("smoothingLength",smoothingLength);
+		Shader.SetGlobalFloat("smoothingLength2",Mathf.Pow(smoothingLength,2));
 		Shader.SetGlobalFloat("smoothingLength3",Mathf.Pow(smoothingLength,3));
 		Shader.SetGlobalFloat("smoothingLength6",Mathf.Pow(smoothingLength,6));
+		Shader.SetGlobalFloat ("smoothingLength9", Mathf.Pow (smoothingLength, 9));
 	}
-
-	int ping = 0;
+		
+	int smoothingCount = 500;
 	void OnPostRender(){
         material.SetPass(0);
 		Graphics.DrawProcedural (MeshTopology.Points, npts, 1);
-		if (methods == Methods.sph) {
-			//if (ping == 0) {
-				cloudCompute.Dispatch (csidDensityPressure, nthr, 1, 1);
-			//	ping = 1;
-			//} else {
-				//	GPUFence fence1 =  Graphics.CreateGPUFence ();
-				//	Graphics.WaitOnGPUFence (fence1);
-				cloudCompute.Dispatch (csidPressureForce, nthr, 1, 1);
-			//	ping = 0;
-			//}
-			cloudCompute.Dispatch (csidIntegrate, nthr, 1, 1);
-		//	GPUFence fence2 = Graphics.CreateGPUFence ();
-		//	Graphics.WaitOnGPUFence (fence2);
+
+		if (smoothingCount-- > 0) {
+			cloudCompute.Dispatch (csidSmoothBall, nthr, 1, 1);
+		} else if (methods == Methods.sph) {
+			cloudCompute.Dispatch (csidSPH, nthr, 1, 1);
 		} else if (methods == Methods.power) {
 			cloudCompute.Dispatch (csidPower, nthr, 1, 1);
-			cloudCompute.Dispatch (csidIntegrate, nthr, 1, 1);
 		} else if (methods == Methods.gravity) {
 			cloudCompute.Dispatch (csidGravity, nthr, 1, 1);
+		} else if (methods == Methods.smooth) {
+			cloudCompute.Dispatch (csidSmoothBall, nthr, 1, 1);
 		}
 
     }
--- Assets/sphShader.shader
+++ Assets/sphShader.shader
@@ -42,55 +42,18 @@
 
 			RWStructuredBuffer<Particle> cloud : register(u1);
 
-			#define WALL_DAMPING  0.3
-			#define TIME_STEP 1.0
-			// time step written in as 1, debugging
-
 			v2f vert (uint id : SV_VertexID)
 			{
 				v2f o;
 				Particle p = cloud[id];
 				o.vertex = UnityObjectToClipPos(p.pos);
-
-				float3 v_n = p.vel;
-				float3 p_n = p.pos;
-				/**
-				if (p_n.x < -100) { 
-					p_n.x = -100;
-					v_n.x *= -1 * WALL_DAMPING;
-				}
-				if (p_n.x > 100) { 
-					p_n.x = 100;
-					v_n.x *= -1 * WALL_DAMPING;
-				}
-				if (p_n.y < -100) { 
-					p_n.y = -100;
-					v_n.y *= -1 * WALL_DAMPING;
-				}
-				if (p_n.y > 100) { 
-					p_n.y = 100;
-					v_n.y *= -1 * WALL_DAMPING;
-				}
-				if (p_n.z < -100) { 
-					p_n.z = -100;
-					v_n.z *= -1 * WALL_DAMPING;
-				}
-				if (p_n.z > 100) { 
-					p_n.z = 100;
-					v_n.z *= -1 * WALL_DAMPING;
-				}
-				**/
-				cloud[id].vel = v_n;
-				cloud[id].pos = p_n;
 				o.color = cloud[id].color;
 				return o;
 			}
 			
 			fixed4 frag (v2f i) : SV_Target
 			{
-				// sample the texture
 				fixed4 col = fixed4(i.color,1); 
-				// col.r = 255;
 				return col;
 			}
 			ENDCG
